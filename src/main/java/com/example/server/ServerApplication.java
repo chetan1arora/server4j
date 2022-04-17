776f0533126ae749f37319bfa2fbc07e9d7c62f2
lolcoder <chetan.arora02@gmail.com>
2022-04-17 09:54:30 +0530

    Registry client + BOM changes

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -1,25 +1,32 @@
 package com.example.server;
 
-
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+//import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.boot.web.servlet.ServletRegistrationBean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @ComponentScan
+@EnableDiscoveryClient
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);		
 	}
 
+	
 	 @Bean
-	    public ServletRegistrationBean servletRegistrationBean() {
-	        ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/*");
-	        registration.setName("CamelServlet");
-	        return registration;
-	    }
+	 public ServletRegistrationBean<CamelHttpTransportServlet> servletRegistrationBean() {
+	 ServletRegistrationBean<CamelHttpTransportServlet> registration = new
+	 ServletRegistrationBean<CamelHttpTransportServlet>(new
+	 CamelHttpTransportServlet(), "/camel/*"); registration.setName("CamelServlet");
+	return registration; }
+	
 }
--- src/main/java/com/example/server/routes/MainRouter.java
+++ src/main/java/com/example/server/routes/MainRouter.java
@@ -1,8 +1,6 @@
 package com.example.server.routes;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
@@ -12,19 +10,14 @@ public class MainRouter extends RouteBuilder{
 	
 	@Override
 	public void configure() throws Exception {
-		
-		  
-		
+
 		restConfiguration()
 		.component("servlet")
-		.contextPath("/")
-		.apiContextPath("/api-doc")
 		.host("localhost")
-		.port(8080);
+		.port(8081);
 		
 		rest("/ping")
 		.get()
-		.produces(MediaType.TEXT_PLAIN_VALUE)
 		.route()
 		.transform().constant("pong!!");
 
